6f02f597c837f65440da5e4918cc00579fea77d5
AlanDuran17 <alanduran1993@gmail.com>
2024-09-25 19:53:51 -0300

    Added Unversioned Files

---
+++ src/main/java/com/alanduran/spring_recipes_app/SpringRecipesAppApplication.java
@@ -0,0 +1,13 @@
+package com.alanduran.spring_recipes_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRecipesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRecipesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alanduran/spring_recipes_app/SpringRecipesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alanduran.spring_recipes_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecipesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
