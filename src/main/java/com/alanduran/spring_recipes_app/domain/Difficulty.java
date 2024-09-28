acb157965f07adf559430a048d414b13fe25aea9
AlanDuran17 <alanduran1993@gmail.com>
2024-09-27 23:41:50 -0300

    136. JPA enumerations - Difficulty

---
+++ src/main/java/com/alanduran/spring_recipes_app/domain/Difficulty.java
@@ -0,0 +1,6 @@
+package com.alanduran.spring_recipes_app.domain;
+
+public enum Difficulty {
+
+    EASY, MODERATE, HARD
+}
--- src/main/java/com/alanduran/spring_recipes_app/domain/Recipe.java
+++ src/main/java/com/alanduran/spring_recipes_app/domain/Recipe.java
@@ -28,4 +28,7 @@ public class Recipe {
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
+
+    @Enumerated(EnumType.STRING)
+    private Difficulty difficulty;
 }
