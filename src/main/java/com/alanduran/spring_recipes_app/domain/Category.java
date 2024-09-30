398d3b67006ac1022a3f2555640ae1fb3569374e
AlanDuran17 <alanduran1993@gmail.com>
2024-09-29 21:40:33 -0300

    137. Many to Many JPA Relationships

---
+++ src/main/java/com/alanduran/spring_recipes_app/domain/Category.java
@@ -0,0 +1,19 @@
+package com.alanduran.spring_recipes_app.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Set;
+
+@Entity
+@Data
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+}
